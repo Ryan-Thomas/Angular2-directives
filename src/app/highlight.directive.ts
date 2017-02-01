509dcd762a03875d089fcac40927908e95e6ce3a
Ryan Thomas <ryanthomas970@gmail.com>
2017-01-31 23:20:53 -0800

    Completed 040 Element Interaction with HostListener and HostBinding

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,4 +1,4 @@
-import {Directive, ElementRef, Renderer} from '@angular/core';
+import {Directive, ElementRef, Renderer, HostListener, HostBinding} from '@angular/core';
 // ElementRef tells the Angular 2 Framework: "When you create this directive, I need
 // this argument of type ElementRef"
 
@@ -6,14 +6,27 @@ import {Directive, ElementRef, Renderer} from '@angular/core';
   selector: '[dirHighlight]'
 })
 export class HighlightDirective {
+  private backgroundColor = 'white';
 
-  constructor(private elementRef: ElementRef, private renderer: Renderer) {
+  @HostListener('mouseenter') mouseover() {
+    this.backgroundColor = 'green';
+  }
+
+  @HostListener('mouseleave') mouseleave() {
+    this.backgroundColor = 'white';
+  }
+
+  @HostBinding('style.backgroundColor') get setColor() {
+    return this.backgroundColor;
+  }
+
+  constructor() {
     // the nativeElement is just an HTML element, which is why we can access its style
     // this.elementRef.nativeElement.style.backgroundColor = 'green';
     // The previous line is considered bad style because there is a better way
 
     // This is the better way: Use a renderer
-    this.renderer.setElementStyle(this.elementRef.nativeElement, 'background-color', 'green');
+    // this.renderer.setElementStyle(this.elementRef.nativeElement, 'background-color', 'green');
   }
 
 }
